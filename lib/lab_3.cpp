3f8cf5e7b2e170c0e3f8c9e7f456ddf222bb8be1
GSemix <63607553+GSemix@users.noreply.github.com>
2021-12-12 21:37:42 +0300

    v 7.0

---
+++ lib/lab_3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <ctime>
+#include "../include/lab_3.h"
+#include "../include/deck.h"
+
+std::string suitCardToString (suitCard suit) {
+    switch (suit) {
+        case Diamonds:
+            return "Diamonds";
+
+        case Hearts:
+            return "Hearts";
+
+        case Clubs:
+             return "Clubs";
+
+        case Spades:
+            return "Spades";
+
+        case Default:
+            return "Default";
+    }
+}
+
+int getRandomNumber(int min, int max) { // Выбирает случайное число от min до max
+    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+    // Равномерно распределяем рандомное число в нашем диапазоне
+    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
